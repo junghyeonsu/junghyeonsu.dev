1052fe1396bf00b80875500895f05a95af53c26f
정현수 <jung660317@naver.com>
2022-03-17 22:58:16 +0900

    chore: 마크다운 리더기, 블로그의 아주 기본적인 형식 갖추기
    - markdownToHtml을 통해서 마크다운을 HTML로 변경
    - index 페이지에서 포스트들을 받아서 렌더링
    - 포스트를 클릭하면 해당 포스트의 자세한 내용을 보여줌

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,34 @@
 import Link from 'next/link';
+import { getAllPosts } from '../lib/api';
+import Post from '../types/post';
 
-const IndexPage = () => (
-  <>
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
+interface Props {
+  allPosts: Post[];
+}
+
+const IndexPage = ({ allPosts }: Props) => {
+  const { title, slug, date, author, coverImage } = allPosts[0];
+
+  return (
+    <>
+      <Link as={`/posts/${slug}`} href="/posts/[slug]">
+        <div>
+          <img style={{ width: '100px', height: '100px', objectFit: 'contain' }} src={coverImage} />
+          <h1>{title}</h1>
+          <p>{date}</p>
+          <p>{author.name}</p>
+        </div>
       </Link>
-    </p>
-  </>
-);
+    </>
+  );
+};
 
 export default IndexPage;
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts(['title', 'date', 'slug', 'author', 'coverImage', 'excerpt']);
+
+  return {
+    props: { allPosts },
+  };
+};
---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,83 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { getAllPosts, getPostBySlug } from '../../lib/api';
+import markdownToHtml from '../../lib/markdownToHtml';
+import PostType from '../../types/post';
+
+type Props = {
+  post: PostType;
+  preview?: boolean;
+};
+
+const Post = ({ post, preview }: Props) => {
+  const router = useRouter();
+  if (!router.isFallback && !post?.slug) {
+    return <div>statusCode 404</div>;
+  }
+
+  return (
+    <div>
+      <header>헤더</header>
+      {router.isFallback ? (
+        <div>Loading…</div>
+      ) : (
+        <article>
+          <Head>
+            <title>{post.title} | 현수의 블로그</title>
+            <meta property="og:image" content={post.ogImage.url} />
+          </Head>
+          <div>
+            post header
+            <div>{post.title}</div>
+          </div>
+          <div>{post.content}</div>
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default Post;
+
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function getStaticProps({ params }: Params) {
+  const post = getPostBySlug(params.slug, [
+    'title',
+    'date',
+    'slug',
+    'author',
+    'content',
+    'ogImage',
+    'coverImage',
+  ]);
+  const content = await markdownToHtml(post.content || '');
+
+  return {
+    props: {
+      post: {
+        ...post,
+        content,
+      },
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts(['slug']);
+
+  return {
+    paths: posts.map(post => {
+      return {
+        params: {
+          slug: post.slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
+}
