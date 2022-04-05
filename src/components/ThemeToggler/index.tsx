1c5f9ccc9febbf4f794e98209f13ca2ec41a2c0c
정현수 <jung660317@naver.com>
2022-04-05 16:05:50 +0900

    style: 테마 토글 버튼 스타일링

--- src/components/ThemeToggler/index.tsx
+++ src/components/ThemeToggler/index.tsx
@@ -1,33 +1,28 @@
-import {
-  IconButton,
-  IconButtonProps,
-  useColorMode,
-  useColorModeValue,
-  Button,
-} from '@chakra-ui/react';
+import { IconButton, IconButtonProps, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import MoonIcon from './MoonIcon';
 import SunIcon from './SunIcon';
 
 type ThemeModeTogglerProps = Omit<IconButtonProps, 'aria-label'>;
 
 const ThemeToggler: React.FC<ThemeModeTogglerProps> = () => {
-  const { toggleColorMode } = useColorMode();
+  const { toggleColorMode, colorMode } = useColorMode();
   const SwitchIcon = useColorModeValue(MoonIcon, SunIcon);
 
   return (
-    <>
-      <IconButton
-        fontSize="2xl"
-        variant="solid"
-        onClick={() => toggleColorMode()}
-        icon={<SwitchIcon />}
-        _hover={{ bg: 'transparent' }}
-        _active={{ bg: 'transparent' }}
-        style={{ boxShadow: 'none' }}
-        aria-label="darkmode toggle button"
-      />
-      <Button>버튼</Button>
-    </>
+    <IconButton
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      fontSize="2xl"
+      borderRadius="3xl"
+      variant="unstyled"
+      onClick={() => toggleColorMode()}
+      icon={<SwitchIcon />}
+      _hover={{ bg: colorMode === 'dark' ? 'whiteAlpha.200' : 'blackAlpha.200' }}
+      _active={{ bg: 'transparent' }}
+      style={{ boxShadow: 'none' }}
+      aria-label="darkmode toggle button"
+    />
   );
 };
 
