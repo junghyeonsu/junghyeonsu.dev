0bc96754728f23650484f1daca8b02d282bc3273
정현수 <jung660317@naver.com>
2022-04-05 15:41:15 +0900

    chore: 폰트 변경
    - Noto Sans KR로 변경
    - weight은 300, 500, 700으로 사용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,11 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;500;700&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
