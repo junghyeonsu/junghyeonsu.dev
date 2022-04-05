b75a200c308669ee3f7c15a8b67df9875fde0a43
정현수 <jung660317@naver.com>
2022-04-05 15:56:25 +0900

    feat: Footer 내용 & 스타일 변경

--- src/components/PostContentContainer/index.tsx
+++ src/components/PostContentContainer/index.tsx
@@ -6,14 +6,7 @@ interface Props {
 
 const PostContentContainer = ({ children }: Props) => {
   return (
-    <Box
-      as="article"
-      position="relative"
-      width="800px"
-      padding="7"
-      marginTop="50"
-      marginBottom="130"
-    >
+    <Box as="article" position="relative" width={800} padding={7} marginTop={50} marginBottom={30}>
       {children}
     </Box>
   );
