efc83ba624c3f7072fc68f765617b14082c620a0
정현수 <jung660317@naver.com>
2022-04-06 22:18:43 +0900

    chore: Footer 태그 변경

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import { Center } from '@chakra-ui/react';
 
 const Footer = () => {
   return (
-    <Center overflow="hidden" width="100%" height={100} fontSize={12}>
+    <Center as="footer" overflow="hidden" width="100%" height={100} fontSize={12}>
       Copyright {new Date().getFullYear()}. junghyeonsu all rights reserved.
     </Center>
   );
