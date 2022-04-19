ed9d8070df58c90ba8f687523f436f5049e0d2db
정현수 <jung660317@naver.com>
2022-04-19 17:04:01 +0900

    bug: giscus 제대로 동작안하는 것 수정

--- src/components/Giscus/index.tsx
+++ src/components/Giscus/index.tsx
@@ -1,24 +1,42 @@
-import Giscus from '@giscus/react';
 import { useColorMode } from '@chakra-ui/react';
+import { useCallback, useEffect } from 'react';
+
+const COMMENTS_ID = 'comments-container';
 
 const GiscusComponent = (): JSX.Element => {
   const { colorMode } = useColorMode();
 
-  return (
-    <Giscus
-      id="comments"
-      repo="junghyeonsu/junghyeonsu.dev.comment"
-      repoId="R_kgDOHH1JSg"
-      category="Announcements"
-      categoryId="DIC_kwDOHH1JSs4COp9e"
-      mapping="title"
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="bottom"
-      theme={colorMode === 'dark' ? 'dark_dimmed' : 'light_protanopia'}
-      lang="ko"
-    />
-  );
+  const LoadComments = useCallback(() => {
+    const script = document.createElement('script');
+    script.src = 'https://giscus.app/client.js';
+    script.setAttribute('data-repo', 'junghyeonsu/junghyeonsu.dev.comment');
+    script.setAttribute('data-repo-id', 'R_kgDOHH1JSg');
+    script.setAttribute('data-category', 'Announcements');
+    script.setAttribute('data-category-id', 'DIC_kwDOHH1JSs4COp9e');
+    script.setAttribute('data-mapping', 'title');
+    script.setAttribute('data-reactions-enabled', '1');
+    script.setAttribute('data-emit-metadata', '0');
+    script.setAttribute('data-input-position', 'bottom');
+    script.setAttribute('data-lang', 'ko');
+    script.setAttribute('data-theme', colorMode === 'dark' ? 'dark_dimmed' : 'light_protanopia');
+    script.setAttribute('crossorigin', 'anonymous');
+    script.async = true;
+
+    const comments = document.getElementById(COMMENTS_ID);
+    if (comments) comments.appendChild(script);
+
+    return () => {
+      const comments = document.getElementById(COMMENTS_ID);
+      if (comments) comments.innerHTML = '';
+    };
+  }, [colorMode]);
+
+  // Reload on theme change
+  useEffect(() => {
+    LoadComments();
+  }, [LoadComments]);
+
+  return <div className="giscus" id={COMMENTS_ID} />;
 };
 
 export default GiscusComponent;
