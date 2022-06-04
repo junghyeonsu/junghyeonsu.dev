97a6944d365ae23cee62e7442e867df78fc6c1c9
정현수 <jung660317@naver.com>
2022-06-04 12:46:45 +0900

    chore: 메인페이지 image 경로 변경

--- src/components/CustomHead/index.tsx
+++ src/components/CustomHead/index.tsx
@@ -45,15 +45,16 @@ const CustomHead = ({ post, type }: CustomHeadInterface) => {
       {/* HTML Meta Tags */}
       <title>정현수 기술 블로그</title>
       <meta name="description" content="공부한 것들을 정리해서 올립니다." />
-
       {/* Facebook Meta Tags */}
       <meta property="og:url" content="https://junghyeonsu-dev.vercel.app/" />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="정현수 기술 블로그" />
       <meta property="og:title" content="정현수 기술 블로그" />
       <meta property="og:description" content="공부한 것들을 정리해서 올립니다." />
-      <meta property="og:image" content={`${DOMAIN}/blob/main/public/profile.jpeg?raw=true`} />
-
+      <meta
+        property="og:image"
+        content="https://github.com/junghyeonsu/junghyeonsu.dev//blob/main/public/profile.jpeg?raw=true"
+      />
       {/*  Twitter Meta Tags  */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="junghyeonsu-dev.vercel.app" />
@@ -62,8 +63,8 @@ const CustomHead = ({ post, type }: CustomHeadInterface) => {
       <meta name="twitter:description" content="공부한 것들을 정리해서 올립니다." />
       <meta
         name="twitter:image"
-        content={`${DOMAIN}/blob/main/public/profile.jpeg?raw=true`}
-      ></meta>
+        content="https://github.com/junghyeonsu/junghyeonsu.dev//blob/main/public/profile.jpeg?raw=true"
+      />
       <meta name="twitter:label1" content="Category" />
       <meta name="twitter:data1" content="개발" />
     </Head>
