f5ccade1b540c482886ce3458f78d11b24a35ba8
정현수 <jung660317@naver.com>
2022-06-04 14:33:51 +0900

    feat: 채널톡 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,6 +30,19 @@ export default class MyDocument extends Document {
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
           <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: String.raw` (function() { var w = window; if (w.ChannelIO) { return (window.console.error || window.console.log ||
+              function(){})('ChannelIO script included twice.'); } var ch = function() { ch.c(arguments); }; ch.q = []; ch.c = function(args) {
+              ch.q.push(args); }; w.ChannelIO = ch; function l() { if (w.ChannelIOInitialized) { return; } w.ChannelIOInitialized = true; var s =
+              document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+              s.charset = 'UTF-8'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); } if (document.readyState ===
+              'complete') { l(); } else if (window.attachEvent) { window.attachEvent('onload', l); } else {
+              window.addEventListener('DOMContentLoaded', l, false); window.addEventListener('load', l, false); } })(); ChannelIO('boot', {
+              "pluginKey": "${process.env.NEXT_PUBLIC_CHANNAL_IO_API_KEY}" });`,
+            }}
+          />
+          <script
             dangerouslySetInnerHTML={{
               __html: `
                 window.dataLayer = window.dataLayer || []; function
