b465c8cb710b8641691cc576616b7ae068f50cc7
junghyeonsu <jung660317@naver.com>
2022-12-31 20:50:08 +0900

    style: tags 스타일링

--- src/framer-motions.ts
+++ src/framer-motions.ts
@@ -1,21 +1,15 @@
 export const fadeInFromLeft = {
   initial: {
     opacity: 0,
-    x: -10,
+    x: -20,
   },
   animate: {
     opacity: 1,
     x: 0,
   },
-};
-
-export const fadeInFromTop = {
-  initial: {
-    opacity: 0,
-    y: -10,
-  },
-  animate: {
-    opacity: 1,
-    y: 0,
+  transition: {
+    type: "spring",
+    stiffness: 100,
+    damping: 20,
   },
 };
