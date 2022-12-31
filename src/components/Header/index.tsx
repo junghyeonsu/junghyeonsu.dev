463578ed655a4ad413bd66885809ac2fce6aeb3d
junghyeonsu <jung660317@naver.com>
2022-12-31 19:21:07 +0900

    style: 헤더 디자인

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Spacer } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import RSS from "../RSS";
 import ThemeToggler from "../ThemeToggler/ThemeToggler";
@@ -7,6 +7,24 @@ import About from "./About";
 import Logo from "./Logo";
 
 const Header = () => {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <Box
       overflow="hidden"
@@ -21,7 +39,8 @@ const Header = () => {
       alignItems="center"
       width="100%"
       height="70px"
-      shadow="md"
+      transition="box-shadow 0.3s ease"
+      shadow={isSticky ? "sm" : "none"}
       backgroundColor="white"
       _dark={{
         backgroundColor: "gray.800",
