411664637a4030bcba48e57e88ace1276eefe3e2
junghyeonsu <jung660317@naver.com>
2023-01-01 15:33:28 +0900

    feat: 그리드 디자인

--- src/components/MainPostCard.tsx
+++ src/components/MainPostCard.tsx
@@ -1,4 +1,4 @@
-import { Badge, Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Center, Flex, Heading, Text } from "@chakra-ui/react";
 import { Link } from "gatsby";
 import type { IGatsbyImageData } from "gatsby-plugin-image";
 import { GatsbyImage } from "gatsby-plugin-image";
@@ -6,7 +6,7 @@ import { useMemo } from "react";
 
 import { koreanTagNames } from "../constants";
 
-interface PostCardProps {
+interface MainPostCardProps {
   title: string;
   description: string;
   slug: string;
@@ -24,7 +24,7 @@ const MainPostCard = ({
   thumbnail,
   title,
   updatedAt,
-}: PostCardProps) => {
+}: MainPostCardProps) => {
   const diffMs = useMemo(() => new Date().getTime() - new Date(createdAt).getTime(), [createdAt]);
   const isNewPost = useMemo(() => Math.floor(diffMs / (1000 * 60 * 60 * 24)) <= 10, [diffMs]);
 
@@ -32,12 +32,13 @@ const MainPostCard = ({
     <Link to={`/posts/${slug}`}>
       <Box
         as="article"
-        transition="box-shadow 0.25s ease"
+        transition="all 0.25s ease"
         position="relative"
         _hover={{ boxShadow: "lg", cursor: "pointer" }}
-        borderRadius="6px"
+        borderRadius="20px"
         overflow="hidden"
         width={{ base: "100%", md: "100%" }}
+        height={{ base: "100%", sm: "280px", md: "340px" }}
       >
         {/* Overlay */}
         <Box
@@ -46,18 +47,106 @@ const MainPostCard = ({
           left={0}
           width="100%"
           height="100%"
-          backgroundColor={"blackAlpha.500"}
-          zIndex={1}
-        />
+          backgroundColor={"blackAlpha.600"}
+          zIndex={2}
+          transition="opacity 0.25s ease"
+          opacity={{ base: 1, md: 0 }}
+          _hover={{
+            opacity: 1,
+          }}
+        >
+          {/* Title */}
+          <Flex
+            position="absolute"
+            bottom={0}
+            left={0}
+            margin="20px"
+            direction="column"
+            alignItems="start"
+          >
+            <Heading fontSize={24} fontWeight="800" noOfLines={1} color={"white"}>
+              {title}
+            </Heading>
+            <Text fontSize={16} color="white" noOfLines={2}>
+              {description}
+            </Text>
+          </Flex>
+
+          {/* Right Top Arrow Icon */}
+          <Center
+            position="absolute"
+            top={0}
+            right={0}
+            margin="20px"
+            backgroundColor="white"
+            borderRadius="50%"
+            width="40px"
+            height="40px"
+          >
+            <svg
+              fill="#000000"
+              width="30px"
+              height="30px"
+              viewBox="-6 -6.5 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMinYMin"
+            >
+              <path d="M7.828 2.414H2.243a1 1 0 1 1 0-2h8a.997.997 0 0 1 1 1v8a1 1 0 0 1-2 0V3.828l-6.779 6.779A1 1 0 0 1 1.05 9.192l6.778-6.778z" />
+            </svg>
+          </Center>
+
+          {/* Date + Tag */}
+          <Flex position="absolute" direction="column" left={0} top={0} margin="20px" gap="10px">
+            <Box
+              backgroundColor="white"
+              borderRadius="20px"
+              padding="10px"
+              fontSize="16px"
+              fontWeight="800"
+              width="fit-content"
+            >
+              {updatedAt ? `${updatedAt} (updated)` : createdAt}
+            </Box>
+            {tags?.map((tag) => (
+              <Box
+                key={tag}
+                border="white 2px solid"
+                borderRadius="20px"
+                color="white"
+                padding="10px"
+                fontSize="14px"
+                fontWeight="800"
+                width="fit-content"
+              >
+                {koreanTagNames[tag!]}
+              </Box>
+            ))}
+          </Flex>
+        </Box>
+
+        {/* New Tag */}
+        {isNewPost && (
+          <Box
+            position="absolute"
+            bottom={0}
+            right={0}
+            color="blue.900"
+            borderColor="blue.900"
+            border="3px solid"
+            borderRadius="20px"
+            padding="10px"
+            fontSize="14px"
+            fontWeight="800"
+            margin="20px"
+            width="fit-content"
+            zIndex={1}
+          >
+            NEW POST
+          </Box>
+        )}
 
         {/* Image */}
-        <Box
-          display="block"
-          as="span"
-          width="100%"
-          height={{ base: "100%", md: "340px" }}
-          borderRadius={2}
-        >
+        <Box display="block" as="span" width="100%" height="100%" borderRadius={2}>
           <GatsbyImage
             objectFit="cover"
             style={{ height: "100%" }}
@@ -65,49 +154,6 @@ const MainPostCard = ({
             alt={`${slug} cover image`}
           />
         </Box>
-
-        {/* Title */}
-        <Flex
-          position="absolute"
-          bottom="30px"
-          margin="10px"
-          left={0}
-          direction="column"
-          alignItems="start"
-          zIndex={2}
-        >
-          <Heading fontSize={24} fontWeight="800" noOfLines={1} color={"white"}>
-            {title}
-          </Heading>
-          <Text fontSize={16} color="white" noOfLines={2}>
-            {description}
-          </Text>
-        </Flex>
-
-        {/* Date + Tag + isNew? */}
-        <Flex
-          position="absolute"
-          left={0}
-          bottom={0}
-          direction="column"
-          justifyContent="space-between"
-          padding={2}
-          zIndex={2}
-        >
-          <Box display="flex" columnGap="10px">
-            <Badge fontSize={14}>{updatedAt ? `${updatedAt} (updated)` : createdAt}</Badge>
-            {tags?.map((tag) => (
-              <Badge key={tag} fontSize={14}>
-                {koreanTagNames[tag!]}
-              </Badge>
-            ))}
-            {isNewPost && (
-              <Badge fontSize={14} colorScheme="green">
-                new
-              </Badge>
-            )}
-          </Box>
-        </Flex>
       </Box>
     </Link>
   );
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,8 +1,8 @@
-import { Badge, Box, Flex, Heading, Text, useColorMode } from "@chakra-ui/react";
+import { Box, Center, Flex, Heading, Text } from "@chakra-ui/react";
 import { Link } from "gatsby";
 import type { IGatsbyImageData } from "gatsby-plugin-image";
 import { GatsbyImage } from "gatsby-plugin-image";
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 
 import { koreanTagNames } from "../constants";
 
@@ -25,9 +25,6 @@ const PostCard = ({
   title,
   updatedAt,
 }: PostCardProps) => {
-  const { colorMode } = useColorMode();
-
-  const isDarkMode = useMemo(() => colorMode === "dark", [colorMode]);
   const diffMs = useMemo(() => new Date().getTime() - new Date(createdAt).getTime(), [createdAt]);
   const isNewPost = useMemo(() => Math.floor(diffMs / (1000 * 60 * 60 * 24)) <= 10, [diffMs]);
 
@@ -35,19 +32,116 @@ const PostCard = ({
     <Link to={`/posts/${slug}`}>
       <Box
         as="article"
-        boxShadow="md"
         transition="box-shadow 0.25s ease"
+        position="relative"
         _hover={{ boxShadow: "lg", cursor: "pointer" }}
-        borderRadius="6px"
+        borderRadius="20px"
         overflow="hidden"
+        width={{ base: "100%", md: "100%" }}
+        height={{ base: "100%", sm: "280px", md: "340px" }}
       >
+        {/* Overlay */}
         <Box
-          display="block"
-          as="span"
+          position="absolute"
+          top={0}
+          left={0}
           width="100%"
-          height={{ base: "100%", md: "240px" }}
-          borderRadius={2}
+          height="100%"
+          backgroundColor={"blackAlpha.600"}
+          zIndex={2}
         >
+          {/* Title */}
+          <Flex
+            position="absolute"
+            bottom={0}
+            left={0}
+            margin="20px"
+            direction="column"
+            alignItems="start"
+          >
+            <Heading fontSize={24} fontWeight="800" noOfLines={1} color={"white"}>
+              {title}
+            </Heading>
+            <Text fontSize={16} color="white" noOfLines={2}>
+              {description}
+            </Text>
+          </Flex>
+
+          {/* Right Top Arrow Icon */}
+          <Center
+            position="absolute"
+            top={0}
+            right={0}
+            margin="20px"
+            backgroundColor="white"
+            borderRadius="50%"
+            width="40px"
+            height="40px"
+          >
+            <svg
+              fill="#000000"
+              width="30px"
+              height="30px"
+              viewBox="-6 -6.5 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMinYMin"
+            >
+              <path d="M7.828 2.414H2.243a1 1 0 1 1 0-2h8a.997.997 0 0 1 1 1v8a1 1 0 0 1-2 0V3.828l-6.779 6.779A1 1 0 0 1 1.05 9.192l6.778-6.778z" />
+            </svg>
+          </Center>
+
+          {/* Date + Tag */}
+          <Flex position="absolute" direction="column" left={0} top={0} margin="20px" gap="10px">
+            <Box
+              backgroundColor="white"
+              borderRadius="20px"
+              padding="10px"
+              fontSize="16px"
+              fontWeight="800"
+              width="fit-content"
+            >
+              {updatedAt ? `${updatedAt} (updated)` : createdAt}
+            </Box>
+            {tags?.map((tag) => (
+              <Box
+                key={tag}
+                border="white 2px solid"
+                borderRadius="20px"
+                color="white"
+                padding="10px"
+                fontSize="14px"
+                fontWeight="800"
+                width="fit-content"
+              >
+                {koreanTagNames[tag!]}
+              </Box>
+            ))}
+          </Flex>
+        </Box>
+
+        {/* New Tag */}
+        {isNewPost && (
+          <Box
+            position="absolute"
+            bottom={0}
+            right={0}
+            color="blue.900"
+            borderColor="blue.900"
+            border="3px solid"
+            borderRadius="20px"
+            padding="10px"
+            fontSize="14px"
+            fontWeight="800"
+            margin="20px"
+            width="fit-content"
+            zIndex={1}
+          >
+            NEW POST
+          </Box>
+        )}
+
+        {/* Image */}
+        <Box display="block" as="span" width="100%" height="100%" borderRadius={2}>
           <GatsbyImage
             objectFit="cover"
             style={{ height: "100%" }}
@@ -55,31 +149,6 @@ const PostCard = ({
             alt={`${slug} cover image`}
           />
         </Box>
-        <Flex direction="column" justifyContent="space-between" minH={130} padding={2}>
-          <Flex direction="column">
-            <Heading marginTop={2} fontSize={24} noOfLines={1}>
-              {title}
-            </Heading>
-
-            <Text fontSize={16} color={isDarkMode ? "whiteAlpha.600" : "gray.600"} noOfLines={2}>
-              {description}
-            </Text>
-          </Flex>
-
-          <Box display="flex" columnGap="10px">
-            <Badge fontSize={14}>{updatedAt ? `${updatedAt} (updated)` : createdAt}</Badge>
-            {tags?.map((tag) => (
-              <Badge key={tag} fontSize={14}>
-                {koreanTagNames[tag!]}
-              </Badge>
-            ))}
-            {isNewPost && (
-              <Badge fontSize={14} colorScheme="green">
-                new
-              </Badge>
-            )}
-          </Box>
-        </Flex>
       </Box>
     </Link>
   );
