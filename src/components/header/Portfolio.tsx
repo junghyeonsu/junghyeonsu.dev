ec74a527d433c58152bbb3bf2a1d1bb602a30931
junghyeonsu <jung660317@naver.com>
2023-12-15 16:07:15 +0900

    feat: 나의 다짐

--- src/components/header/Portfolio.tsx
+++ src/components/header/Portfolio.tsx
@@ -1,21 +1,34 @@
-import { Text, Tooltip } from "@chakra-ui/react";
+import { Box, Text, Tooltip } from "@chakra-ui/react";
 import { Link } from "gatsby";
 
 const Portfoilo = () => {
   return (
     <Link to="/portfolio">
-      <Tooltip label="Coming Soon!">
+      <Box pos="relative">
         <Text
-          as="del"
-          fontSize={14}
+          pos="absolute"
           fontStyle="italic"
+          whiteSpace="nowrap"
+          top="-10px"
+          left="-10px"
+          fontSize={10}
           fontWeight={800}
-          padding={1}
-          _active={{ bg: "transparent" }}
         >
-          Portfolio
+          2023년 안에는 꼭...
         </Text>
-      </Tooltip>
+        <Tooltip label="Coming Soon!">
+          <Text
+            as="del"
+            fontSize={14}
+            fontStyle="italic"
+            fontWeight={800}
+            padding={1}
+            _active={{ bg: "transparent" }}
+          >
+            Portfolio
+          </Text>
+        </Tooltip>
+      </Box>
     </Link>
   );
 };
